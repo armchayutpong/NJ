89d8e8542ecae001d75e850cbeca55d71d30bf1c
Chayutpong <43534783+armchayutpong@users.noreply.github.com>
2023-12-10 15:52:09 +0700

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: "localhost",
-  PORT: "1434",
-  USER: "zkoder",
-  PASSWORD: "123456",
-  DB: "bezkoder_db",
+  HOST: "thanvasucloud.com",
+  PORT: "1433",
+  USER: "uinet",
+  PASSWORD: "p@$$w0rd",
+  DB: "CFS_TBC_Restaurant",
   dialect: "mssql",
   pool: {
     max: 5,
